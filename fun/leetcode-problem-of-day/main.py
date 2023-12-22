d703f60eec098e531c59d0c8f7079aeb597c7b95
Jeff Corpuz <jeffcorpuz@users.noreply.github.com>
2023-12-22 15:09:46 -0500

    add lambda possibility

--- fun/leetcode-problem-of-day/main.py
+++ fun/leetcode-problem-of-day/main.py
@@ -1,8 +1,8 @@
+import os
 import requests
-import discord
 import asyncio
 import aiohttp
-import os
+import discord
 
 from dotenv import load_dotenv
 
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Discord Webhook URL
-WEBHOOK_URL: str | None = os.getenv(key="DISCORD_WEBHOOK_URL")
+WEBHOOK_URL: str | None = os.getenv("DISCORD_WEBHOOK_URL")
 
 # LeetCode Random Question URL
 LEETCODE_RANDOM_URL = "https://leetcode.com/problems/random-one-question/"
@@ -25,9 +25,16 @@ async def send_webhook(message) -> None:
         webhook = discord.Webhook.from_url(WEBHOOK_URL, session=session)
         await webhook.send(content=message, username='leetcode-bot')
 
-async def main() -> None:
+async def main(event, context) -> None:
     random_question: str = get_random_question()
     await send_webhook(message=random_question)
 
+# AWS Lambda handler
+def lambda_handler(event, context):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main(event, context))
+
 if __name__ == "__main__":
-    asyncio.run(main=main())
+    # For local testing
+    asyncio.run(main(None, None))
