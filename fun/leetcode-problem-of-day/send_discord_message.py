c7eb88573f48964e36825236ac2ab13fe1adfa0f
Jeff Corpuz <jeffcorpuz@users.noreply.github.com>
2023-12-22 14:18:04 -0500

    changes for discord message

--- fun/leetcode-problem-of-day/send_discord_message.py
+++ fun/leetcode-problem-of-day/send_discord_message.py
@@ -1,6 +1,5 @@
 import requests
 import discord
-from bs4 import BeautifulSoup
 
 # Discord Webhook URL
 WEBHOOK_URL = "https://discord.com/api/webhooks/1187825630339944652/uk1TEq-Ikr2zyHnni7YFJjuruxp2N3CvY9hSKCeSKe7KXverLoApCgiAm4DEBF2_1T5k"
@@ -10,6 +9,7 @@ LEETCODE_RANDOM_URL = "https://leetcode.com/problems/random-one-question/"
 
 def get_random_question():
     response = requests.get(LEETCODE_RANDOM_URL)
+    return response.url
 
 def send_webhook(message):
     webhook = discord.Webhook.from_url(WEBHOOK_URL, adapter=discord.RequestsWebhookAdapter())
