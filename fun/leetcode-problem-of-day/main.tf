62d4e3fa15a5207caa3320f350e038fccb5f6687
Jeff Corpuz <jeffcorpuz@icloud.com>
2023-12-23 18:04:06 -0500

    update things

---
+++ fun/leetcode-problem-of-day/main.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_role" "this" {
+  name = "leetcode_bot_iam"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/files/"
+  output_path = "${path.module}/.terraform/archive_files/lambda.zip"
+}
+
+resource "aws_lambda_function" "this" {
+  function_name = "leetcode-bot"
+  role          = "${aws_iam_role.this.arn}"
+  handler       = "lambda"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  
+  runtime     = "python3.11"
+  memory_size = 128
+  timeout     = 120 # in seconds
+}
